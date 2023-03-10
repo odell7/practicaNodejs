0507569bf8ad747d4f8227139011c5c357bb77f8
odell7 <odellsuarez@gmail.com>
2023-03-10 13:55:08 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import indexRoutes from "./routes/index.js";
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(indexRoutes);
+app.use(express.static(join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.status(404).render("E404");
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("Server is listening on port", process.env.PORT || 3000);
